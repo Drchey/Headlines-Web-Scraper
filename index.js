88bd0f05913901d2d93aaa078ce5c3a2fbe6e32f
drchey <derichey9@gmail.com>
2022-05-24 08:20:18 +0100

    convert to objects

--- index.js
+++ index.js
@@ -2,26 +2,202 @@ const axios = require('axios')
 const cheerio = require('cheerio') 
 const express = require('express')
 
-async function getNews(){
+async function getGDP(){
     try {
         // define the site url we are scraping from 
-        const washingtonPost = 'https://www.washingtonpost.com/world/ukraine-russia/'
+        const gdpPerCapita = 'https://worldpopulationreview.com/countries/countries-by-gdp'
         const data1 = await axios({
             method: 'GET',
-            url: washingtonPost
+            url: gdpPerCapita
         })
 
         const $ = cheerio.load(data1.data)
-        const elemSelector = '#main-content > div:nth-child(1) > div > div.table-in-grid.hpgrid.hpgrid-item.hpgrid-item--c-start.hpgrid-item--c-spans.hpgrid-item--r-spans.extra-wide-left-layout.grid-top.grid-bottom.grid-right.table9.include-dividers-features.hide-helpers > div.card.relative.hpgrid-item.hpgrid-item--c-start.hpgrid-item--c-spans.hpgrid-item--r-spans.table9-childStyles.grid-top.grid-left.grid-right > div > div > div.headline.relative.gray-darkest.pb-xs > h2'
+        const elemSelector = '#worldCountries > div.container.is-fluid.m-0 > div > div > div:nth-child(2) > div.datatable-container.undefined > table > tbody > tr'
+           const keys=[
+            'Rank',
+            'Name',
+            'GDP (IMF 19)',
+            'GDP (UN 16)',
+            'GDP per Capita',
+            'Population',
+        ]
+        
         $(elemSelector).each((parentIdx,parentElem) =>{
-            console.log(parentIdx)
-        })        
+            // define a key index
+            let keyIdx =0
+            let gdpObj = {}
+            if(parentIdx <= 14){  
+                $(parentElem).children().each((childIdx, childElem) => {
+                    const values = $(childElem).text()
+                    if(values){
+                        gdpObj[keys[keyIdx]] = values
+                        keyIdx++
+                    }
+                })
+                console.log(gdpObj)
+            }
+        })
+        // console.log(emi)
+
+
+        
     } catch (err) {
         console.error(err)
     }
 }
 
-getNews()
+async function getHealthCare(){
+    try {
+        // define the site url we are scraping from 
+        const getHealthCare = 'https://worldpopulationreview.com/country-rankings/best-healthcare-in-the-world'
+        const data2 = await axios({
+            method: 'GET',
+            url: getHealthCare
+        })
+
+        const $ = cheerio.load(data2.data)
+        const elemSelector = '#dataTable > div.container.is-fluid.m-0 > div > div.column.is-8.is-clearfix.py-6 > div:nth-child(2) > div.datatable-container.undefined > table > tbody > tr'
+           // const keys=[
+        //     'rank',
+        //     'name',
+        //     'price',
+        //     '24h',
+        //     '7d',
+        //     'MarketCap',
+
+
+        // ]
+        
+        $(elemSelector).each((parentIdx,parentElem) =>{
+            if(parentIdx <= 15){  
+                console.log(parentIdx)            
+            }
+        })
+        // console.log(emi)
+
+
+        
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+async function getPoorNations(){
+    try {
+        // define the site url we are scraping from 
+        const getPoorNation = 'https://worldpopulationreview.com/country-rankings/best-healthcare-in-the-world'
+        const data3 = await axios({
+            method: 'GET',
+            url: getPoorNation
+        })
+
+        const $ = cheerio.load(data3.data)
+        const elemSelector = '#dataTable > div.container.is-fluid.m-0 > div > div.column.is-8.is-clearfix.py-6 > div:nth-child(1) > div.datatable-container.undefined > table > tbody > tr'
+           // const keys=[
+        //     'rank',
+        //     'name',
+        //     'price',
+        //     '24h',
+        //     '7d',
+        //     'MarketCap',
+
+
+        // ]
+        
+        $(elemSelector).each((parentIdx,parentElem) =>{
+            if(parentIdx <= 15){  
+                console.log(parentIdx)            
+            }
+        })
+        // console.log(emi)
+
+
+        
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
+async function getEmissions(){
+    try {
+        // define the site url we are scraping from 
+        const getEmission = 'https://worldpopulationreview.com/country-rankings/co2-emissions-by-country'
+        const data4 = await axios({
+            method: 'GET',
+            url: getEmission
+        })
+
+        const $ = cheerio.load(data4.data)
+        const elemSelector = '#dataTable > div.container.is-fluid.m-0 > div > div.column.is-8.is-clearfix.py-6 > div:nth-child(2) > div.datatable-container.undefined > table > tbody > tr'
+           // const keys=[
+        //     'rank',
+        //     'name',
+        //     'price',
+        //     '24h',
+        //     '7d',
+        //     'MarketCap',
+
+
+        // ]
+        
+        $(elemSelector).each((parentIdx,parentElem) =>{
+            if(parentIdx <= 15){  
+                console.log(parentIdx)            
+            }
+        })
+        // console.log(emi)
+
+
+        
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+async function getCrimeRate(){
+    try {
+        // define the site url we are scraping from 
+        const getCrime = 'https://worldpopulationreview.com/country-rankings/crime-rate-by-country'
+        const data5 = await axios({
+            method: 'GET',
+            url: getCrime
+        })
+
+        const $ = cheerio.load(data5.data)
+        const elemSelector = '#dataTable > div.container.is-fluid.m-0 > div > div.column.is-8.is-clearfix.py-6 > div:nth-child(1) > div.datatable-container.undefined > table > tbody > tr'
+           // const keys=[
+        //     'rank',
+        //     'name',
+        //     'price',
+        //     '24h',
+        //     '7d',
+        //     'MarketCap',
+
+
+        // ]
+        
+        $(elemSelector).each((parentIdx,parentElem) =>{
+            if(parentIdx <= 15){  
+                console.log(parentIdx)            
+            }
+        })
+        // console.log(emi)
+
+
+        
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
+getGDP()
+// getHealthCare()
+// getPoorNations()
+// getEmissions()
+// getCrimeRate()
+
 
 
 
